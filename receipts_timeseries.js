fe40e7cf193713619a1e5a55b417280372bd8dc7
Christopher W York <christopher.w.york@gmail.com>
2015-02-06 19:38:42 +0000

    temporary focus timeseries

--- receipts_timeseries.js
+++ receipts_timeseries.js
@@ -4,6 +4,7 @@ mdat.visualization.receipts_timeseries = function() {
 
   var width = 700, // width = 1024,
       height = 150, //height = 768,
+      height2 = 30,
       title = "Receipts timeline",
       cfrp = undefined,
       uid = 0;
@@ -15,27 +16,30 @@ mdat.visualization.receipts_timeseries = function() {
       .group(d3.time.year)
       .reduceSum(function(d) { return d.sold * d.price; });
 
-    var x = d3.time.scale()
-      .range([0, width]);
+    var x = d3.time.scale().range([0, width]),
+        x2 = d3.time.scale().range([0, width]),
+        y = d3.scale.linear().range([height, 0]),
+        y2 = d3.scale.linear().range([height2, 0]);
 
-    var y = d3.scale.linear()
-      .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+    var brush = d3.svg.brush()
+        .x(x2)
+        .on("brush", brushed);
 
     var commasFormatter = d3.format(",.0f");
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .tickFormat(commasFormatter);  
+    var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+        xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+        yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(commasFormatter);
 
     var line = d3.svg.line()
         .interpolate("basis")
         .x(function(d) { return x(d.key); })
         .y(function(d) { return y(d.value); });
+
+    var line2 = d3.svg.line()
+        .interpolate("basis")
+        .x(function(d) { return x2(d.key); })
+        .y(function(d) { return y2(d.value); });
 
     var root = d3.select(this)
         .classed("receipts_timeseries", true);
@@ -43,17 +47,28 @@ mdat.visualization.receipts_timeseries = function() {
     var background = root.append("rect")
         .attr("class", "background")
         .attr("width", width)
+        .attr("height", height + height2);
+
+    // TODO. garbage collect / detect pre-existing defs / other svg roots / everything is wrong...
+    root.append("clipPath")
+        .attr("id", namespace + "_clip")
+      .append("rect")
+        .attr("width", width)
         .attr("height", height);
 
-    var path = root.append("path")
-        .attr("class", "line");
+    var focus = root.append("g")
+        .attr("class", "focus");
 
-    var xAxisElm = root.append("g")
+    focus.append("path")
+        .attr("class", "line")
+        .attr("clip-path", "url(#" + namespace + "_clip)");
+
+    focus.append("g")
         .call(xAxis)
          .attr("class", "x axis")
          .attr("transform", "translate(0," + height + ")");
 
-    var yAxisElm = root.append("g")
+    focus.append("g")
         .call(yAxis)
         .attr("class", "y axis")
       .append("text")
@@ -63,6 +78,25 @@ mdat.visualization.receipts_timeseries = function() {
         .style("text-anchor", "end")
         .text("Recettes totales (L.)");
 
+    var context = root.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(0," + (height + 25) + ")");         // TODO.  proper margins?  axes hang down from border
+
+    context.append("path")
+        .attr("class", "line");
+
+    context.append("g")
+        .call(xAxis2)
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height2 + ")");
+
+    context.append("g")
+        .attr("class", "x brush")
+        .call(brush)
+      .selectAll("rect")
+        .attr("y", -6)
+        .attr("height", height2 + 7);
+
     update();
 
     cfrp.on("change." + namespace, update);
@@ -70,13 +104,36 @@ mdat.visualization.receipts_timeseries = function() {
     function update() {
       var data = receiptsByDate.all();
 
+      // TODO... see brushed()
+      foo = data;
+
       x.domain(d3.extent(data, function(d) { return d.key; }));
       y.domain([0, d3.max(data, function(d) { return d.value; })]);
+      x2.domain(x.domain());
+      y2.domain(y.domain());
 
-      path.attr("d", line(data));
+      focus.select(".line")
+        .datum(data)
+        .attr("d", line);
+      context.select(".line")
+        .datum(data)
+        .attr("d", line2);
 
-      root.select(".x.axis").call(xAxis);
-      root.select(".y.axis").call(yAxis);
+      focus.select(".x.axis").call(xAxis);
+      focus.select(".y.axis").call(yAxis);
+      context.select(".x.axis").call(xAxis2);
+    }
+
+    function brushed() {
+      d3.event.sourceEvent.stopPropagation();
+      x.domain(brush.empty() ? x2.domain() : brush.extent());
+
+      // TODO.  data is over-ridden by mdat component list..
+      //        how to do this without pushing all data up to
+      //        mdat level?
+
+      focus.select(".line").datum(foo).attr("d", line);
+      focus.select(".x.axis").call(xAxis);
     }
   }
 
